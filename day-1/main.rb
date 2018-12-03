d6c026ea5ac1a0b5b5efd8eb35dbbc18312c4d00
Taylor Jones <taylor.jones@heroku.com>
2018-12-03 14:05:39 -0500

    [DAY-1] - Part 1 Finished; Start of Part 2

---
+++ day-1/main.rb
@@ -0,0 +1,36 @@
+chunks = []
+count_map = {}
+two_times = nil
+
+count = 0
+
+File.open("input.txt", "r") do |file|
+  file.each_line do |line|
+    chunks.push(line)
+  end
+end
+
+
+chunks.each do |chunk|
+    if chunk[0] == "+"
+        count += chunk[1..-1].to_i
+    else 
+        count -= chunk[1..-1].to_i
+    end
+   
+    if count_map.key?(count)
+        count_map[count] += 1
+    else 
+        count_map[count] = 1
+    end
+
+    if two_times.nil? && count_map[count] == 2
+        two_times = count 
+    end
+end
+
+puts count_map.values.uniq
+
+puts "\n\n"
+puts "Final count: #{count}"
+puts "First result to come twice: #{two_times}"
