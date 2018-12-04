393b4f861e721907bd57736455da7c31f32bcde1
Taylor Jones <taylor.jones@heroku.com>
2018-12-04 11:04:35 -0500

    adding test.txt; messing with stuff a bit more

--- day-1/main.rb
+++ day-1/main.rb
@@ -1,7 +1,6 @@
 chunks = []
 count_map = {}
 two_times = nil
-
 count = 0
 
 File.open("input.txt", "r") do |file|
@@ -12,24 +11,33 @@ end
 
 
 chunks.each do |chunk|
-    if chunk[0] == "+"
-        count += chunk[1..-1].to_igit ad
-    else 
-        count -= chunk[1..-1].to_i
-    end
-   
+
     if count_map.key?(count)
         count_map[count] += 1
     else 
         count_map[count] = 1
     end
 
-    if two_times.nil? && count_map[count] == 2
-        two_times = count 
+    if chunk[0] == "+"
+        count += chunk[1..-1].to_i
+    else 
+        count -= chunk[1..-1].to_i
     end
+    
+    
 end
 
-puts count_map.values.uniq
+if count_map.key?(count)
+    count_map[count] += 1
+else 
+    count_map[count] = 1
+end
+
+if two_times.nil? && count_map[count] == 2
+    two_times = count 
+end
+
+puts count_map
 
 puts "\n\n"
 puts "Final count: #{count}"
