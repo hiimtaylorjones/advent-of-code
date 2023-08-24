b7293ab2c72dda8976237e934c873680b6655508
hiimtaylorjones <taylorjonessoftware@gmail.com>
2023-08-23 22:10:05 -0400

    [Day 2] - pt 1

--- 2022/two/main.rb
+++ 2022/two/main.rb
@@ -1,19 +1,36 @@
-path = "test.txt"
-# path = "input.txt"
+# path = "test.txt"
+path = "input.txt"
 
-winning_moves = {
-  # A & X -> Rock
-  # B & Y -> Paper
-  # C & Z -> Scissors
-  "A" => "Y", # 1 point
-  "B" => "Z", # 2 points
-  "C" => "X"  # 3 points
+#   A & X -> Rock     (1 point)
+#   B & Y -> Paper    (2 points)
+#   C & Z -> Scissors (3 points)
+#   Win -> 6
+#   Loss -> 0
+#   Draw -> 3
+
+scenarios = {
+  # Loss
+  "B X" => 1,
+  "C Y" => 2,
+  "A Z" => 3,
+  # Draw
+  "A X" => 4,
+  "B Y" => 5,
+  "C Z" => 6,
+  # Win
+  "C X" => 7,
+  "A Y" => 8,
+  "B Z" => 9
 }
 
-rounds = []
+round_scores = []
 
 File.open(path, "r") do |reader|
   reader.each_line do |line|
-    moves = line.split(" ")
+    trimmed = line.gsub("\n", "")
+    round_scores.push(scenarios[trimmed])
   end
 end
+
+puts "#{round_scores}"
+puts round_scores.sum
